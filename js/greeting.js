3b231a6c1de37ff59121dc77d009b76bfd485075
le yang <50788385+leCloudy@users.noreply.github.com>
2021-04-12 19:11:56 +0800

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' leyang';
+const name = 'leyang';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
